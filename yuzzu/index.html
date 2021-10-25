1ce2214f4f1343dc7e5d4c10a67ccd1e0854c6ef
ユズ - 綺麗な <86514883+YuzzuKamiyaka@users.noreply.github.com>
2021-10-25 10:54:22 +0800

    Add files via upload

---
+++ yuzzu/index.html
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Upload File</title> 
+</head>
+
+<body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        Pilih file: <input type="file" name="berkas" />
+        <input type="submit" name="upload" value="upload" />
+    </form> 
+</body> 
+</html>
---
+++ yuzzu/upload.php
@@ -0,0 +1,20 @@
+<?php
+
+// ambil data file
+$namaFile = $_FILES['berkas']['name'];
+$namaSementara = $_FILES['berkas']['tmp_name'];
+
+// tentukan lokasi file akan dipindahkan
+$dirUpload = "terupload/";
+
+// pindahkan file
+$terupload = move_uploaded_file($namaSementara, $dirUpload.$namaFile);
+
+if ($terupload) {
+    echo "Upload berhasil!<br/>";
+    echo "Link: <a href='".$dirUpload.$namaFile."'>".$namaFile."</a>";
+} else {
+    echo "Upload Gagal!";
+}
+
+?>
